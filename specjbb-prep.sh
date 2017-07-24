5b4ce7ad736a3c50e3f2a1d949e6ac6b46f478f7
Zhihui Chen <chenzhihui4@huawei.com>
2017-07-24 14:19:27 +0800

    update prep.sh

--- specjbb-prep.sh
+++ specjbb-prep.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 yum install -y fontconfig, libXfont, libfontenc, ttmkfdir, xorg-x11-font-utils,xorg-x11-fonts-Type1
-
 yum install -y freetype-devel
+
+cat <<EOF > env.sh
+export JAVA_HOME=/root/jdk1.8.0_131
+export PATH=$PATH:$JAVA_HOME
+export CLASSPATH=$JAVA_HOME/lib
+EOF
