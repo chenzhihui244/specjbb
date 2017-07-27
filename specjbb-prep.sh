89cd2c430c00a1491dda672f83501e9751035c98
Zhihui Chen <chenzhihui4@huawei.com>
2017-07-27 11:05:05 +0800

    improve test scripts

--- specjbb-prep.sh
+++ specjbb-prep.sh
@@ -4,10 +4,14 @@ yum install -y fontconfig, libXfont, libfontenc, ttmkfdir, xorg-x11-font-utils,x
 yum install -y freetype-devel
 yum install -y numactl
 
-cat <<-EOF > env.sh
+cp run_multi_4groupok.sh ..
+cp run_multi_1groupok.sh ..
+cp specjbb-test.sh ..
+
+cat <<-EOF > ../env.sh
 #!/bin/bash
 
 export JAVA_HOME=/root/jdk1.8.0_131
-export PATH=\$PATH:\$JAVA_HOME/bin
-export CLASSPATH=\$JAVA_HOME/lib
+export PATH=\$JAVA_HOME/bin:\$PATH
+export CLASSPATH=.:\${JAVA_HOME}/lib/dt.jar:\${JAVA_HOME}/lib/tools.jar:\$JAVA_HOME/lib
 EOF
